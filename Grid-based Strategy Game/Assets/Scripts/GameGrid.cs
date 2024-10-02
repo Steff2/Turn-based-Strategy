d6e478ba71ad9cfbb6fb6ac538f1af4feed76335
Stefan <stmepp@online.de>
2024-10-02 16:55:31 +0200

    created GameGrid class with generics, no visuals in grid currently

---
+++ Grid-based Strategy Game/Assets/Scripts/GameGrid.cs
@@ -0,0 +1,108 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameGrid<TGridObject>
+{
+    private int width;
+    private int height;
+    private float cellSize;
+    private Vector3 originPosition;
+    private TGridObject[,] gridArray;
+
+    public event EventHandler<OnGridObjectChangedEventArgs> OnGridObjectChanged;
+    public class OnGridObjectChangedEventArgs : EventArgs
+    {
+        public int x;
+        public int y;
+    }
+    public GameGrid(int width, int height, float cellSize, Vector3 originPosition, Func<TGridObject> createGridObject)
+    {
+        this.width = width;
+        this.height = height;
+        this.cellSize = cellSize;
+        this.originPosition = originPosition;
+        
+        gridArray = new TGridObject[width, height];
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                gridArray[x, y] = createGridObject();
+                Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x + 1, y), Color.white, 100f);
+                Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x, y + 1), Color.white, 100f);
+            }
+        }
+
+        Debug.DrawLine(GetWorldPosition(0, height), GetWorldPosition(width, height), Color.white, 100f);
+        Debug.DrawLine(GetWorldPosition(width, 0), GetWorldPosition(width, height), Color.white, 100f);
+
+        // TODO
+        OnGridObjectChanged += (object sender, OnGridObjectChangedEventArgs eventArgs) =>
+        {
+            gridArray[eventArgs.x, eventArgs.y]?.ToString();
+        };
+    }
+
+    public int GetWidth()
+    {
+        return width;
+    }
+
+    public int GetHeight()
+    {
+        return height;
+    }
+
+    public float GetCellSize()
+    {
+        return cellSize;
+    }
+
+    public Vector3 GetWorldPosition(int x, int y)
+    {
+        return new Vector3 (x, y) * cellSize + originPosition;
+    }
+
+    private void GetXY(Vector3 worldPosition, out int x, out int y)
+    {
+        x = Mathf.FloorToInt((worldPosition - originPosition).x / cellSize);
+        y = Mathf.FloorToInt((worldPosition - originPosition).y / cellSize);
+    }
+
+    public void SetGridObject(int x, int y, TGridObject value)
+    {
+        if(x < width  && y < height && x >= 0 && y >= 0)
+        {
+            gridArray[x,y] = value;
+        }
+    }
+
+    public void SetGridObject(Vector3 worldPosition, TGridObject value)
+    {
+        int x, y;
+        GetXY(worldPosition, out x, out y);
+        SetGridObject(x, y, value);
+    }
+
+    public TGridObject GetGridObject(int x, int y)
+    {
+        if (x < width && y < height && x >= 0 && y >= 0)
+        {
+            return gridArray[x, y];
+        }
+        else
+        {
+            return default;
+        }
+    }
+    
+    public TGridObject GetGridObject(Vector3 worldPosition)
+    {
+        GetXY(worldPosition, out int x, out int y);
+        return GetGridObject(x, y);
+    }
+
+}
