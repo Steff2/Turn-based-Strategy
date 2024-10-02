82bc52b8d8423898173f4648f5fb952ced695959
Stefan <stmepp@online.de>
2024-10-02 16:54:51 +0200

    created utils class for further use, current with world mouse pos

---
+++ Grid-based Strategy Game/Assets/Scripts/GameUtils.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace Utils
+{
+    public class GameUtils : MonoBehaviour
+    {
+        // Get Mouse Position in World with Z = 0f
+        public static Vector3 GetMouseWorldPosition()
+        {
+            Vector3 vec = GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
+            vec.z = 0f;
+            return vec;
+        }
+        public static Vector3 GetMouseWorldPositionWithZ()
+        {
+            return GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
+        }
+        public static Vector3 GetMouseWorldPositionWithZ(Camera worldCamera)
+        {
+            return GetMouseWorldPositionWithZ(Input.mousePosition, worldCamera);
+        }
+        public static Vector3 GetMouseWorldPositionWithZ(Vector3 screenPosition, Camera worldCamera)
+        {
+            Vector3 worldPosition = worldCamera.ScreenToWorldPoint(screenPosition);
+            return worldPosition;
+        }
+    }
+}
