2a9329a39c57da0be7cfddca9dd0c5dcc84d18db
Stefan <stmepp@online.de>
2024-10-11 14:13:01 +0200

    added over additional utils from codemonkey utils to own util

--- Grid-based Strategy Game/Assets/Scripts/GameUtils.cs
+++ Grid-based Strategy Game/Assets/Scripts/GameUtils.cs
@@ -3,11 +3,15 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 namespace Utils
 {
-    public class GameUtils : MonoBehaviour
+    public static class GameUtils
     {
+
+        public const int sortingOrderDefault = 5000;
+
         // Get Mouse Position in World with Z = 0f
         public static Vector3 GetMouseWorldPosition()
         {
@@ -27,6 +31,51 @@ namespace Utils
         {
             Vector3 worldPosition = worldCamera.ScreenToWorldPoint(screenPosition);
             return worldPosition;
+        }
+
+        // Create a Sprite in the World, no parent
+        public static GameObject CreateWorldSprite(string name, Sprite sprite, Vector3 position, Vector3 localScale, int sortingOrder, Color color)
+        {
+            return CreateWorldSprite(null, name, sprite, position, localScale, sortingOrder, color);
+        }
+
+        // Create a Sprite in the World
+        public static GameObject CreateWorldSprite(Transform parent, string name, Sprite sprite, Vector3 localPosition, Vector3 localScale, int sortingOrder, Color color)
+        {
+            GameObject gameObject = new GameObject(name, typeof(SpriteRenderer));
+            Transform transform = gameObject.transform;
+            transform.SetParent(parent, false);
+            transform.localPosition = localPosition;
+            transform.localScale = localScale;
+            SpriteRenderer spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+            spriteRenderer.sprite = sprite;
+            spriteRenderer.sortingOrder = sortingOrder;
+            spriteRenderer.color = color;
+            return gameObject;
+        }
+
+        // Create Text in the World
+        public static TextMesh CreateWorldText(string text, Transform parent = null, Vector3 localPosition = default(Vector3), int fontSize = 40, Color? color = null, TextAnchor textAnchor = TextAnchor.UpperLeft, TextAlignment textAlignment = TextAlignment.Left, int sortingOrder = sortingOrderDefault)
+        {
+            if (color == null) color = Color.white;
+            return CreateWorldText(parent, text, localPosition, fontSize, (Color)color, textAnchor, textAlignment, sortingOrder);
+        }
+
+        // Create Text in the World
+        public static TextMesh CreateWorldText(Transform parent, string text, Vector3 localPosition, int fontSize, Color color, TextAnchor textAnchor, TextAlignment textAlignment, int sortingOrder)
+        {
+            GameObject gameObject = new GameObject("World_Text", typeof(TextMesh));
+            Transform transform = gameObject.transform;
+            transform.SetParent(parent, false);
+            transform.localPosition = localPosition;
+            TextMesh textMesh = gameObject.GetComponent<TextMesh>();
+            textMesh.anchor = textAnchor;
+            textMesh.alignment = textAlignment;
+            textMesh.text = text;
+            textMesh.fontSize = fontSize;
+            textMesh.color = color;
+            textMesh.GetComponent<MeshRenderer>().sortingOrder = sortingOrder;
+            return textMesh;
         }
     }
 }
