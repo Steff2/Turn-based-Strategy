6c7da282d7902477b554bae2a898376f2427ad32
Stefan <stmepp@online.de>
2024-10-14 17:20:05 +0200

    added Get Node, Get Grid, FindPath (for vector3 input), changed initial gCost value to 9999999, integrated isWalkable, fixed neighbourcost checking

--- Grid-based Strategy Game/Assets/Scripts/Pathfinding.cs
+++ Grid-based Strategy Game/Assets/Scripts/Pathfinding.cs
@@ -8,13 +8,49 @@ public class Pathfinding
 {
     private const int MOVE_STRAIGHT_COST = 10;
     private const int MOVE_DIAGONAL_COST = 14;
+
+    public static Pathfinding Instance {  get; private set; }
+
     private GameGrid<PathNode> grid;
     private List<PathNode> openList;
     private HashSet<PathNode> closedList;
 
+
     public Pathfinding(int width, int height)
     {
-        grid = new GameGrid<PathNode> (width, height, 10f, Vector3.zero, (GameGrid<PathNode> g, int x, int y) => new PathNode(g, x, y));
+        Instance = this;
+        grid = new GameGrid<PathNode>(width, height, 10f, Vector3.zero, (GameGrid<PathNode> g, int x, int y) => new PathNode(g, x, y));
+    }
+
+    public GameGrid<PathNode> GetGrid()
+    {
+        return grid;
+    }
+
+    public PathNode GetNode(int x, int y)
+    {
+        return grid.GetGridObject(x, y);
+    }
+
+    public List<Vector3> FindPath(Vector3 startWorldPosition, Vector3 endWorldPosition)
+    {
+        grid.GetXY(startWorldPosition, out int startX, out int startY);
+        grid.GetXY(endWorldPosition, out int endX, out int endY);
+
+        List<PathNode> path = FindPath(startX, startY, endX, endY);
+        if(path == null)
+        {
+            return null;
+        }
+        else
+        {
+            List<Vector3> vectorPath = new List<Vector3>();
+            foreach(PathNode pathNode in path)
+            {
+                vectorPath.Add(new Vector3(pathNode.x, pathNode.y) * grid.GetCellSize() + .5f * grid.GetCellSize() * new Vector3(1, 1));
+            }
+            return vectorPath;
+        }
     }
 
     public List<PathNode> FindPath(int startX, int startY, int endX, int endY)
@@ -30,7 +66,7 @@ public class Pathfinding
             for (int y = 0; y < grid.GetHeight(); y++)
             {
                 PathNode pathNode = grid.GetGridObject(x, y);
-                pathNode.gCost = int.MaxValue;
+                pathNode.gCost = 99999999;
                 pathNode.CalculateFCost();
                 pathNode.cameFromNode = null;
             }
@@ -54,10 +90,14 @@ public class Pathfinding
             foreach (PathNode neighbourNode in GetNeighourList(currentNode))
             {
                 if (closedList.Contains(neighbourNode)) continue;
+                if (!neighbourNode.isWalkable)
+                {
+                    closedList.Add(neighbourNode);
+                    continue;
+                }
 
                 int tentativeGCost = currentNode.gCost + CalculateDistanceCost(currentNode, neighbourNode);
-
-                if(tentativeGCost < currentNode.gCost)
+                if(tentativeGCost < neighbourNode.gCost)
                 {
                     neighbourNode.cameFromNode = currentNode;
                     neighbourNode.gCost = tentativeGCost;
@@ -110,7 +150,7 @@ public class Pathfinding
     {
         List<PathNode> path = new() { endNode };
         PathNode currentNode = endNode;
-        while(currentNode != null)
+        while(currentNode.cameFromNode != null)
         {
             path.Add(currentNode.cameFromNode);
             currentNode = currentNode.cameFromNode;
