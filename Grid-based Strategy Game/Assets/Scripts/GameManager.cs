1f39f4be774561b477df5871ae7327c48cbc48b6
Stefan <stmepp@online.de>
2024-11-13 17:08:06 +0100

    function for returning pathfinding sys

--- Grid-based Strategy Game/Assets/Scripts/GameManager.cs
+++ Grid-based Strategy Game/Assets/Scripts/GameManager.cs
@@ -47,5 +47,10 @@ namespace GridCombat
         { 
             return grid;
         }
+
+        public Pathfinding GetPathfinding() 
+        { 
+            return pathfindingGrid;
+        }
     }
 }
