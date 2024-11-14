a2c954d3bfe573577b3a49a280176b1ec3fd720d
Stefan <stmepp@online.de>
2024-11-14 17:48:12 +0100

    added BinaryTree GetRightMostLeaf, changed Delete to Remove, added AddNode

--- Grid-based Strategy Game/Assets/Scripts/DataStructures/BinaryTree.cs
+++ Grid-based Strategy Game/Assets/Scripts/DataStructures/BinaryTree.cs
@@ -1,50 +1,161 @@
-
-using System;
+using UnityEngine;
 
 public class BinaryTree
 {
-    public TreeNode Search(TreeNode root, int value)
-    {
-        if (root == null || root.value == value)
-        {
-            return root;
-        }
-        
-        if (root.value < value)
-        {
-            return Search(root.right, value);
-        }
 
-        return Search(root.left, value);
+    public class TreeNode
+    {
+        public TreeNode parentNode;
+        public TreeNode leftTreeNode;
+        public TreeNode rightTreeNode;
+        public PathNode pathNode;
+
+        public TreeNode(PathNode pathNode, TreeNode leftNode, TreeNode rightNode)
+        {
+            this.pathNode = pathNode;
+            leftTreeNode = leftNode;
+            rightTreeNode = rightNode;
+        }
     }
 
-    public TreeNode Insert(TreeNode root, int value)
+    private TreeNode root;
+    public int nodeCount = 0;
+
+    public BinaryTree()
     {
-        if (root == null)
-        {
-            root = new TreeNode(value);
-            return root;
-        }
-
-        if (root.value < value)
-        {
-            return Search(root.right, value);
-        }
-        else if(root.value > value)
-        {
-            return Search(root.left, value);
-        }
-
-        return root;
+        root = null;
     }
 
-    public TreeNode Delete(TreeNode root, int value)
+    public TreeNode Search(TreeNode treeNode, PathNode pathNode)
     {
-        if (root == null)
+        if(treeNode == null)
         {
             return null;
         }
-        return root;
+        else if(treeNode.pathNode == pathNode)
+        {
+            return treeNode;
+        }
+        
+        if (root.pathNode.fCost < pathNode.fCost)
+        {
+            return Search(root.rightTreeNode, pathNode);
+        }
+        else
+        {
+            return Search(root.leftTreeNode, pathNode);
+        }
     }
+    public void AddNode(PathNode pathNode)
+    {
+        //debugCodePath += "AD; ";
+        if (root == null)
+        {
+            root = new TreeNode(pathNode, null, null);
+            nodeCount++;
+        }
+        else
+        {
+            // Has root
+            try
+            {
+                Insert(root, pathNode, pathNode.fCost);
+            }
+            catch (System.Exception e)
+            {
+                Debug.Log(e);
+            }
+            nodeCount++;
+        }
+    }
+    public void Insert(TreeNode treeNode, PathNode pathNode, int fCost)
+    {
+        if (treeNode.pathNode.fCost < pathNode.fCost)
+        {
 
+            if (treeNode == null)
+            {
+                treeNode.rightTreeNode = new TreeNode(pathNode, null, null);
+                treeNode.rightTreeNode.parentNode = treeNode;
+            }
+            else
+            {
+                Insert(treeNode.rightTreeNode, pathNode, fCost);
+            }
+
+        }
+        else if (treeNode.pathNode.fCost > pathNode.fCost)
+        {
+            if (treeNode == null)
+            {
+                treeNode.leftTreeNode = new TreeNode(pathNode, null, null);
+                treeNode.leftTreeNode.parentNode = treeNode;
+            }
+            else
+            {
+                Insert(treeNode.leftTreeNode, pathNode, fCost);
+            }
+        }
+    }
+    public void RemoveNode(PathNode pathNode)
+    {
+        var nodeToBeRemoved = Search(root, pathNode);
+        Remove(nodeToBeRemoved, nodeToBeRemoved.pathNode);
+    }
+    private void Remove(TreeNode treeNode, PathNode pathNode)
+    {
+        if (pathNode == null)
+        {
+            return;
+        }
+
+        if(treeNode.leftTreeNode == null && treeNode.rightTreeNode == null)
+        {
+            if (root.pathNode == pathNode)
+            {
+                root = null;
+            }
+        }
+        else
+        {
+            var parentRoot = treeNode.parentNode;
+
+            if (treeNode.leftTreeNode == null || treeNode.rightTreeNode == null)
+            {
+                var newChild = treeNode.leftTreeNode ?? treeNode.rightTreeNode;
+                newChild.parentNode = parentRoot;
+
+                if(newChild.leftTreeNode == treeNode)
+                {
+                    parentRoot.leftTreeNode = newChild;
+                }
+                else
+                {
+                    parentRoot.rightTreeNode = newChild;
+                }
+
+                if(treeNode == root)
+                {
+                    root = newChild;
+                }
+            }
+            else
+            {
+                var newChild = GetRightMostLeaf(treeNode);
+                parentRoot.leftTreeNode = newChild;
+                newChild.leftTreeNode = treeNode.leftTreeNode;
+            }
+        }
+    }
+    private TreeNode GetRightMostLeaf(TreeNode node)
+    {
+        var testNode = node;
+        var targetNode = node;
+        while (testNode != null) 
+        {
+            targetNode = testNode;
+            testNode = testNode.rightTreeNode;
+        }
+        return targetNode;
+    }
 }
