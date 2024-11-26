fad7e9155926e3f2ffd5a9653d0025917df6ff2b
Stefan <stmepp@online.de>
2024-11-26 16:03:08 +0100

    AttackUnit function, Attacking state, GetPosition,GetTeam

--- Grid-based Strategy Game/Assets/Scripts/UnitGridCombat.cs
+++ Grid-based Strategy Game/Assets/Scripts/UnitGridCombat.cs
@@ -17,15 +17,19 @@ namespace GridCombat
         public enum State
         {
             Walking,
-            Shooting,
+            Attacking,
             Idle
         }
+        
         //private HealthSystem healthSystem;
         private CharacterMovementHandler characterMovementHandler;
 
         private void Awake()
         {
             characterMovementHandler = gameObject.GetComponent<CharacterMovementHandler>();
+            //healthSystem = new HealthSystem(100);
+            //healthBar = new World_Bar(transform, new Vector3(0, 10), new Vector3(10, 1.3f), Color.grey, Color.red, 1f, 10000, new World_Bar.Outline { color = Color.black, size = .5f });
+            //healthSystem.OnHealthChanged += HealthSystem_OnHealthChanged;
         }
         public void MoveTo(Vector3 targetPos, Action onComplete)
         {
@@ -34,12 +38,37 @@ namespace GridCombat
             {
                 state = State.Idle;
                 onComplete();
-            }); // Needs Action to set state back to normal after reaching destination)
+            });
         }
-
+        public void AttackUnit(UnitGridCombat unitGridCombat, Action onComplete)
+        {
+            state = State.Attacking;
+            Strike(unitGridCombat, () =>
+            {
+                state = State.Idle;
+                onComplete();
+            });
+        }
+        private void Strike(UnitGridCombat unitGridCombat, Action onStrikeComplete)
+        {
+            TakeDamage(UnityEngine.Random.Range(2, 6));
+            onStrikeComplete();
+        }
         public void TakeDamage(float damageTaken)
         {
             //healthSystem.Damage(damageTaken);
+        }
+        /*public bool IsDead()
+        {
+            return healthSystem.IsDead();
+        }*/
+        public Vector3 GetPosition()
+        {
+            return transform.position;
+        }
+        public Team GetTeam()
+        {
+            return team;
         }
     }
 }
