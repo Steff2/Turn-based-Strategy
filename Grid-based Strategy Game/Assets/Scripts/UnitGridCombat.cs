ae9f3ae2fd90aada1d25e612562ff08bbee132f7
Stefan <stmepp@online.de>
2024-11-29 13:39:54 +0100

    added range check and attacking+damaging

--- Grid-based Strategy Game/Assets/Scripts/UnitGridCombat.cs
+++ Grid-based Strategy Game/Assets/Scripts/UnitGridCombat.cs
@@ -7,6 +7,10 @@ namespace GridCombat
     public class UnitGridCombat : MonoBehaviour
     {
         [SerializeField] private Team team;
+        [SerializeField] private bool isRangedAttacker;
+
+        private const float maxAttackRangeMelee = 20;
+        private const float maxAttackRangeShot = 50;
 
         //State state;
         public enum Team
@@ -24,15 +28,19 @@ namespace GridCombat
         private HealthSystem healthSystem;
         private Usable_Bar healthBar;
         private CharacterMovementHandler characterMovementHandler;
+        private float activeAttackRange;
 
         private void Awake()
         {
             characterMovementHandler = gameObject.GetComponent<CharacterMovementHandler>();
-            healthSystem = new HealthSystem(100);
+            healthSystem = new HealthSystem(10);
             healthBar = new Usable_Bar(transform, new Vector3(0, 10), new Vector3(10, 1.3f), Color.grey, Color.red, 1f, 10000, new Usable_Bar.Outline { color = Color.black, size = .5f });
             healthSystem.OnHealthChanged += HealthSystem_OnHealthChanged;
         }
-
+        private void Start()
+        {
+            SetWeaponProperties();
+        }
         private void HealthSystem_OnHealthChanged(object sender, EventArgs e)
         {
             healthBar.SetSize(healthSystem.GetHealthNormalized());
@@ -49,21 +57,70 @@ namespace GridCombat
         }
         public void AttackUnit(UnitGridCombat unitGridCombat, Action onComplete)
         {
-            //state = State.Attacking;
-            Strike(unitGridCombat, () =>
+            if(isRangedAttacker)
             {
-                //state = State.Idle;
-                onComplete();
-            });
+                Shoot(unitGridCombat, () =>
+                {
+                    //state = State.Idle;
+                    onComplete();
+                });
+            }
+            else
+            {
+                Strike(unitGridCombat, () =>
+                {
+                    //state = State.Idle;
+                    onComplete();
+                });
+            }
         }
+
+        private void Shoot(UnitGridCombat unitGridCombat, Action onShotComplete)
+        {
+            unitGridCombat.Damage(UnityEngine.Random.Range(2, 6));
+            onShotComplete();
+        }
+
         private void Strike(UnitGridCombat unitGridCombat, Action onStrikeComplete)
         {
-            TakeDamage(UnityEngine.Random.Range(2, 6));
+            unitGridCombat.Damage(UnityEngine.Random.Range(4, 8));
             onStrikeComplete();
         }
-        public void TakeDamage(float damageTaken)
+
+        public bool IsInRange(UnitGridCombat target)
+        {
+            if (Vector3.Distance(gameObject.transform.position, target.gameObject.transform.position) < activeAttackRange)
+            {
+                return true;
+            }
+
+            return false;
+        }
+        public void SwitchWeapon()
+        {
+            isRangedAttacker = !isRangedAttacker;
+            SetWeaponProperties();
+        }
+
+        private void SetWeaponProperties()
+        {
+            if (isRangedAttacker)
+            {
+                activeAttackRange = maxAttackRangeShot;
+            }
+            else
+            {
+                activeAttackRange = maxAttackRangeMelee;
+            }
+        }
+
+        public void Damage(float damageTaken)
         {
             healthSystem.Damage(damageTaken);
+            if (IsDead())
+            {
+                Destroy(gameObject);
+            }
         }
         public bool IsDead()
         {
@@ -71,7 +128,7 @@ namespace GridCombat
         }
         public bool CheckForEnemy(UnitGridCombat testSubject)
         {
-            return testSubject.GetTeam() == team;
+            return testSubject.GetTeam() != team;
         }
         public Vector3 GetPosition()
         {
